35f52d3a0e9adae4da0f03f3c46a91cdfd9da48a
charlesperrillat <ce.perrillat@gmail.com>
2024-04-17 16:38:44 +0200

    uploading the program on GitHub

---
+++ Building.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace _1_POO_Les_constructeurs
+{
+	public class Building
+	{
+		private int _numberOfFloors;
+		private double _height;
+
+		public Building(int numberOfFloors) : this(numberOfFloors, numberOfFloors * 3)
+		{ }
+
+		public Building(double height) : this((int) (height / 3), height)
+		{ }
+
+        public Building(int numberOfFloors, double height)
+		{
+			_numberOfFloors = numberOfFloors;
+			_height = height;
+        }
+
+		public double GetFloorMaxSize()
+		{
+			return _height / _numberOfFloors;
+		}
+
+		public int GetFloorCount()
+		{
+			return _numberOfFloors;
+		}
+
+		public double GetSize()
+		{
+			return _height;
+		}
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _1_POO_Les_constructeurs;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the height of the building in meters: ");
+        double height = double.Parse(Console.ReadLine());
+
+        Building building = new Building(height);
+        Console.WriteLine($"There will be {building.GetFloorCount()} floors of {building.GetFloorMaxSize()} meters");
+    }
+}
